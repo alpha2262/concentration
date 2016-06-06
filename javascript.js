1ef7b9b896bdf7c7f27c67f7d44bd479938b7965
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-06 09:59:44 -0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+  console.log( "document loaded")
+
+
+
+
+
+
+  });
+
+  //when user selects timer, the timer will start and icons become selectable
+
+  //var for keeping track of what's displayed/not. class show on/off?
+
+
+  //on click, the block displays
+    //on second click, second item displays
+      //do first and second item match?
+        //if yes, continue to display and update variable to show
+        //if no, flip both first and second back to blank after 1 second
+      //are all blocks visible?
+        //if so, game ends
+        //if not, restart matching function
+
+  //on end when all matching, stop timer, alert 'good job!'
+  //prompt to reset game?
